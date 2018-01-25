3b9228fa32ac180c5d20cf7b58754bfd4c02ea04
ToolElf <diu5diu@gmail.com>
2018-01-25 16:18:00 +0800

    first commit

---
+++ Bubble Sort.php
@@ -0,0 +1,14 @@
+<?php
+// 使用for循环，实现冒泡排序：
+$arr = array(5,3,6,2,8,10);
+for($i = count($arr)-1;$i>=0;$i--){
+    for($j = 0 ; $j < $i ; $j++){
+        if($arr[$j+1] > $arr[$j] ){
+            $aa = $arr[$j+1];
+            $arr[$j+1] = $arr[$j];
+            $arr[$j] = $aa;
+        }
+    }
+}
+print_r($arr);
+?>
